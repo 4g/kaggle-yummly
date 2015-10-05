fc8789f292e035b6ca680870c12fba0711ded45d
apurva <apurvagup@gmail.com>
2015-10-05 16:52:34 +0530

    icf and terms

--- create_model.py
+++ create_model.py
@@ -1,32 +1,29 @@
 import argparse
 import scipy
 import scipy.sparse
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.decomposition import PCA
 from tdec import timeit
 from sklearn.cross_validation import train_test_split
 from sklearn import linear_model, datasets, metrics
-from sklearn.cross_validation import train_test_split
-from sklearn.neural_network import BernoulliRBM
-from sklearn.pipeline import Pipeline
-from sklearn.svm import SVC
-from sklearn.svm import LinearSVC
-from sklearn import tree
-import modeML
-from sklearn.feature_selection import chi2
-from sklearn.feature_selection import SelectKBest
+import math
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import *
+import random
 
 uniq_elems = {}
+global_weight = {}
+stop = stopwords.words('english')
+stemmer = PorterStemmer()
 
 def main(train, test, out, mode):
   
   train_f , train_l , train_i = load_vectors(train)
   
   if mode == 'test':
-    train_f, test_f, train_l, test_l = train_test_split(train_f, train_l,
-                                                      test_size=0.2,
-                                                      random_state=0)
+    train_f, test_f, train_l, test_l = train_test_split(train_f, 
+                                                        train_l,
+                                                        test_size = 0.2,
+                                                        random_state  = 0)
   """train the model"""
   model = cmodel(train_f, train_l)
   
@@ -38,7 +35,7 @@ def main(train, test, out, mode):
     test_f , test_l , test_i = load_vectors(test, create_f = False)
     print test_f.shape , len(test_l) , len(test_i)
     out = open(out, 'wb')
-    out.write("id,cuisine\n")
+    out.write("id, cuisine\n")
     for features, ide in zip(test_f, test_i):
       label = model.predict(features)
       string = str(ide) + "," + uniq_elems[label[0]] + "\n"
@@ -55,7 +52,9 @@ def get_accuracy(train_f, train_l, test_f, test_l, model):
 
 @timeit
 def cmodel(f, l):
-  classifier = linear_model.LogisticRegression(multi_class = 'ovr', solver = 'liblinear', verbose = 2, max_iter = 1000)
+  classifier = linear_model.LogisticRegression(multi_class = 'ovr', 
+                                               solver = 'liblinear', 
+                                               max_iter = 1000)
   classifier.fit(f, l)
   return classifier
 
@@ -68,8 +67,7 @@ def load_vectors(fname, create_f = True):
   ids = [] 
   for elem in data:
     i =  elem.get('ingredients',None)
-    i = [e.lower().split() for e in i]
-    i = reduce(lambda x,y:x + y,i)
+    i = tokenize(i)
     c = elem.get('cuisine',None)
     ide = elem.get('id')
     ids += [ide]
@@ -82,24 +80,50 @@ def load_vectors(fname, create_f = True):
     features += [[uniq_elems[a] for a in i]]
     if c != None:
       labels += [uniq_elems[c]]
-    
-  features = to_sparse_matrix(features)  
-#  features , labels = reduce_dim(features, labels)
+  
+  if create_f:
+    calculate_global_weights(features, labels)
+  
+  features = to_sparse_matrix(features)
   return features , labels, ids
 
+def tokenize(i):
+  clean = lambda x:re.sub('[^A-Za-z]', ' ', x)
+  i = [word_tokenize(clean(e).lower()) for e in i]
+  i = reduce(lambda x,y:x + y,i)
+  i = map(stemmer.stem, i)
+  i = filter(lambda x:x not in stop,i)
+  return i
+
 @timeit
-def reduce_dim(f,l):
-  best_f = SelectKBest(chi2, k=100)
-  f = best_f.fit_transform(f, l)
-  return f , l
+def calculate_global_weights(features, labels):
+  total_cuisines = len(set(labels))
+  word_cf = {}
   
+  for f,l in zip(features,labels):
+    for elem in set(f):
+      word_cf[elem] = word_cf.get(elem,[]) + [l]
+   
+  for elem in word_cf:
+#    print map(uniq_elems.get, word_cf[elem]) , uniq_elems[elem]
+    word_cf[elem] = len(set(word_cf[elem]))
+    
+  for elem in word_cf:
+    global_weight[elem] = math.log(1.0 + total_cuisines/word_cf[elem])
+
+#  for elem in global_weight:
+#    print uniq_elems[elem] , global_weight[elem]
+  
+#  for elem in word_df:
+#    print uniq_elems[elem] , word_df[elem] , global_weight[elem]
+
 @timeit
 def to_sparse_matrix(ll):
   w , h = len(ll) , len(uniq_elems)
   mat = scipy.sparse.dok_matrix((w,h))
   for i in range(w):
     for j in ll[i]:
-      mat[i,j] += 1    
+      mat[i,j] += global_weight[j]
   return mat
 
 if __name__ == "__main__":
